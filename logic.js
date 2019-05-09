0f7422a6633918d3e2398053c0774a3079d8c855
Jonathan Behar <jdb443@stern.nyu.edu>
2019-05-09 17:57:08 -0400

    new game

--- logic.js
+++ logic.js
@@ -1,8 +1,8 @@
 // Homework 3 Jonathan Behar
 
 // GameWords Variable
-var gameWords = ["hodor", "stark", "brandon", "winterfell", "samwell", "castle", "thyion", "dragon", "zombie"];
-var allowedchars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "v", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "&", "?"]
+var gameWords = [ "azorahai", "brandon", "castle", "dragon", "greyscale", "hackle", "hellbent", "hodor", "holdthedoor", "ironwood", "jonsnow", "khaleesi", "kingslayer", "lightbringer", "maester", "others", "ranging", "rootout", "samwell", "sire", "stark", "tyrion", "unman", "unsullied", "valyrian", "wall", "warg", "wetnurse","whitewalker", "wights", "wildlings", "winterfell"];
+var allowedchars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "v", "x", "y", "z"]
 
 
 var puzzleSection = document.getElementById("puzzle-state");
@@ -10,8 +10,9 @@ var wrongGuesses = document.getElementById("wrong-guesses");
 var numGuesses = document.getElementById("guesses-left");
 var winCounter = document.getElementById("win-counter");
 var lossCounter = document.getElementById("loss-counter");
-var modelText = document.getElementById("modal-text");
+var modalText = document.getElementById("modal-text");
 
+//mygame function
 var myGame = setupGame(gameWords, 0, 0);
 puzzleSection.innerHTML = printArray(myGame.round.puzzleState);
 
@@ -67,7 +68,8 @@ function updateRound(thisRound, letterGuessed) {
     if (isCorrectGuess(thisRound.word, letterGuessed)) {
         thisRound.puzzleState = fillBlanks(thisRound.word, thisRound.puzzleState, letterGuessed);
     }
-    else {
+    else if(!thisRound.wrongGuesses.includes(letterGuessed) && allowedchars.includes(letterGuessed)) {
+
         console.log("wrong guess");
         thisRound.wrongGuesses.push(letterGuessed);
         thisRound.guessesLeft--;
@@ -117,3 +119,153 @@ function setupGame(randomWords, numWins, numLosses) {
     }
     return game;
 }
+
+// function startNewRound(game) {
+    
+//     if (hasLost(game.round.guessesLeft)) {
+//         game.losses++;
+//         modelText.innerHTML = "You have lost! The characters name was: " + game.round.word;
+//         $("#alert-modal").modal("show");
+
+//     }
+//     else if (hasWon(game.round.puzzleState)) {
+//         game.wins++;
+//         modelText.innerHTML = "You have won! The characters name was: " + game.round.word;
+//         $("#alert-modal").modal("show");
+//     }
+
+//     game.round = setupRound(randomWord(game.words));
+    
+// }
+
+ 
+
+
+// function startNewRound(game) {
+//     var puzzleState = game.round.puzzleState;
+//     if (game.round.puzzleState.indexOf("_") == -1 && game.round.guessesLeft > 0) {
+//         alert("winner " + game.round.word + "!");
+//         game.wins++;
+//         //console.log("win game");
+//         return true;
+//     }
+//     else if (game.puzzleState.indexOf("_") > -1 && game.round.guessesLeft == 0) {
+//         alert("lose" + game.round.word);
+//         game.losses++;
+//         //console.log("lose game");
+//         return false;
+//     }
+// }
+
+function startNewRound(game) {
+    var puzzleState = game.round.puzzleState;
+    if (hasWon(puzzleState) === true) {
+        game.wins++;
+        alert("Correct! To the King in the Seven Kingdoms! The word is " + game.round.word + "!");
+    }
+    else {
+        game.losses++;
+        alert("You Lose! Valar morghulis! The word was " + game.round.word + "!");
+    }
+    game.round = setupRound(randomWord(gameWords));
+}
+
+function printArray(array){
+    var arrayAsString = "";
+    for( var i = 0; i < array.length; i++){
+        if( i === (array.length - 1)){
+            arrayAsString += array[i];
+        }
+        else{
+            arrayAsString += array[i] + " ";
+        }
+        
+    }
+    return arrayAsString;
+}
+
+document.addEventListener( 'keydown', function(e){
+    console.log("letter played: " + e.key);
+
+    var letterPlayed = e.key;
+
+    updateRound(myGame.round, letterPlayed);
+    
+    if(isCorrectGuess(myGame.round.word, letterPlayed)){
+        puzzleSection.innerHTML = printArray(myGame.round.puzzleState);
+
+    }
+    else{
+        wrongGuesses.innerHTML = printArray(myGame.round.wrongGuesses);
+        numGuesses.innerHTML = myGame.round.guessesLeft;
+    }
+
+    if(isEndOfRound(myGame.round)){
+        startNewRound(myGame);
+
+        console.log(myGame.round);
+
+        //Update DOM
+        puzzleSection.innerHTML = printArray(myGame.round.puzzleState);
+        wrongGuesses.innerHTML = myGame.round.wrongGuesses;
+        numGuesses.innerHTML = myGame.round.guessesLeft;
+        winCounter.innerHTML = myGame.wins;
+        lossCounter.innerHTML = myGame.losses;
+ 
+    }   
+
+});
+
+// var keyPressed;
+// document.onkeyup = function (evt) {
+//     keyPressed = evt.key.toLowerCase() 
+//     console.log("The " + keyPressed + " key was pressed");
+
+//     // CALL BACK LOGIC
+//     isCorrectGuess(myGame.round.word, keyPressed);
+//     fillBlanks(myGame.round.word, myGame.round.puzzleState, keyPressed);
+//     updateRound(myGame.round, keyPressed);
+//     hasWon(myGame.round.puzzleState);
+//     hasLost(myGame.round.guessesLeft);
+
+//     // CHECKS IF GUESSES ARE LEFT OR HAS WON
+//     if (isEndOfRound(myGame.round)){
+//         myGame = startNewRound(myGame);
+//         myGame.round = setupRound(randomWord(gameWords));
+//     }
+//     // --------- end CALL BACK THE LOGIC
+
+//     // Uses the ramdom word and displays the empty blanks
+//     document.getElementById("puzzle-state").innerText = myGame.round.puzzleState.join(" ");
+
+//     // Displays the updated object for wrong guesses from user
+//     document.getElementById("wrong-guesses").innerText = myGame.round.wrongGuesses;
+
+//     // Displays the updated object for total wins
+//     document.getElementById("win-counter").innerText = myGame.wins;
+
+//     // Displays the updated object for total losses
+//     document.getElementById("loss-counter").innerText = myGame.losses;
+
+//     // Displays the updated object for number of guesses left
+//     document.getElementById("guesses-left").innerText = myGame.round.guessesLeft;
+
+//     console.log(myGame);
+
+// } // ------------ end of onclick event
+
+
+
+
+// RESET GAME
+function gameReset() {
+    setupRound(myGame.words)
+    hasWon(myGame.round.puzzleState);
+    updateRound(myGame.round, keyPressed);
+    startNewRound(myGame);
+    randomWord(myGame.words);
+    isEndOfRound(myGame.round);
+    hasLost(myGame.round.guessesLeft);
+} // ------------ end of button click
+
+
